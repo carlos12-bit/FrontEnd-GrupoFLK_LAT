f06a911ef4da031ce6e4c92a34c5891580bca092
adrianH0498 <202011420@urp.edu.pe>
2024-09-12 01:29:59 -0500

    Refactor(Home): Update heading and description in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <navbar></navbar>
     <div class="home">
       <div class="jumbotron text-center" style="background-color: #8C3A1C; color: white;">
-        <h1 class="display-4">Inspección de Maquinaria Pesada</h1>
+        <h1 class="display-4">Inspección de Maquinaria Pesada Y ligera</h1>
         <p class="lead">Ofrecemos servicios de inspección para montacargas, grúas y más.</p>
         <hr class="my-4" style="border-color: #BF5630;">
         <router-link to="/services" class="btn btn-primary btn-lg">Nuestros Servicios</router-link>
