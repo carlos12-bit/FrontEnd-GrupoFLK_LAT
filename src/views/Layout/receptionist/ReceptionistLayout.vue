62dad8c58a854930e4bfc9db46e6e3c5bfbe79e8
adrianH0498 <202011420@urp.edu.pe>
2024-10-09 05:12:40 -0500

    Refactor layout components for Admin, Receptionist, and Operations Assistant; remove unused Sidebar and Footer components

--- src/views/Layout/receptionist/ReceptionistLayout.vue
+++ src/views/Layout/receptionist/ReceptionistLayout.vue
@@ -1,1 +1,19 @@
-<template></template>
+<!-- src/layouts/MainLayout.vue -->
+<template>
+    <el-container style="height: 100vh">
+      <Drawer ref="drawer" class="d-md-none" />
+      <el-main>
+        <router-view></router-view> 
+      </el-main>
+    </el-container>
+  </template>
+  
+  <script>
+  import Drawer from './Drawer.vue';
+  
+  export default {
+    components: {
+      Drawer
+    },
+  };
+  </script>
